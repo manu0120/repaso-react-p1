b5ec4dba3aae8c4b83b47a94540584186077ab2d
manu0120 <manu0120sp@gmail.com>
2025-01-09 11:38:56 +0100

    follow card separated in a component

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,29 +1,18 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
-    const profileImageUrl = 'https://unavatar.io/github/37t?fallback=https://source.boringavatars.com/marble/120/1337_user?colors=264653r,2a9d8f,e9c46a,f4a261,e76f51'
-    const name = 'Oscar Varela'
+    // const profileImageUrl = 'https://unavatar.io/github/37t?fallback=https://source.boringavatars.com/marble/120/1337_user?colors=264653r,2a9d8f,e9c46a,f4a261,e76f51'
+    // const profileImageUrl = `https://unavatar.io/${username}`
     const username = 'oscar_varela'
+    const name = 'Oscar Varela'
+    const formatUserName = (userName) => `@${userName}`
 
     return (
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img 
-                    className='tw-followCard-avatar'
-                    src={profileImageUrl} 
-                    alt="profile image" 
-                />
-                <div className='tw-followCard-info'>
-                    <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>@{username}</span>
-                </div>
-            </header>
-
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <div className='App'>
+            <TwitterFollowCard userName={username} name={name} isFollowing={false} formatUserName={formatUserName}/>
+            <TwitterFollowCard userName="mig_hernandez" name="Miguel Hernandez" isFollowing formatUserName={formatUserName}/>
+            <TwitterFollowCard userName="miguel_hernandez" name={name} isFollowing={false} formatUserName={formatUserName}/>
+        </div>
     )
 }
---
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,25 @@
+export function TwitterFollowCard ({ formatUserName, userName, name, isFollowing }){
+    const imageSrc = `https://unavatar.io/${userName}`
+
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img 
+                    className='tw-followCard-avatar'
+                    src={imageSrc} 
+                    alt="profile image" 
+                />
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-infoUserName'>{formatUserName(userName)}</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className='tw-followCard-button'>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
