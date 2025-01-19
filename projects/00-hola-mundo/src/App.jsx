97f6ff4af9a8d569067b7548565189fd4abc3cf3
Manuel <manu0120sp@gmail.com>
2025-01-19 23:58:05 +0100

    lists / arrays rendering

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -15,17 +15,42 @@ export function App() {
     */
     const miguel_hernandez = {userName: "miguel_hernandez", formatUserName: format}
 
+    const users = [
+        {
+            formatUserName: format,
+            userName: "oscarmiguel",
+            name: "Oscar Miguel",
+            initialIsFollowing: false
+        },
+        {
+            formatUserName: format,
+            userName: "mig_hernandez",
+            name: "Miguel Hernández",
+            initialIsFollowing: true
+        },
+        {
+            formatUserName: format,
+            userName: "manuel",
+            name: "Manuel Gallardo",
+            initialIsFollowing: true
+        }
+    ]
+
     return (
         <div className='App'>
+            {users.map((user) => (
+                <TwitterFollowCard {...user} key={user.userName}/>
+            ))}
+
             <TwitterFollowCard userName={username} name={name} formatUserName={format}
                 initialIsFollowing={false}/>
             <TwitterFollowCard userName="mig_hernandez" name="Miguel Hernandez" formatUserName={format} initialIsFollowing/>
             
-            {/* Uso de REST_OPERATOR (no confundir con el spread operator): ℹ No es recomendable usarlo porque
+            {/* Uso de SPREAD OPERATOR (no confundir con el REST operator) https://chatgpt.com/share/678a9f95-38f4-8011-ab19-55c05f31e945: ℹ No es recomendable usarlo porque
             se puede mandar información de más, o porque puede hacer que
             por temas de optimización el componente se rerenderice sin 
             necesidad*/}
-            <TwitterFollowCard { ... miguel_hernandez} initialIsFollowing={false}>
+            <TwitterFollowCard {...miguel_hernandez} initialIsFollowing={false}>
                 <strong>Miguel Hernández</strong>
             </TwitterFollowCard>
         </div>
