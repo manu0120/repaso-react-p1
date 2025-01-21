189eb29fdd76b6abe38c63b0461f60edb018893c
Manuel <manu0120sp@gmail.com>
2025-01-21 23:27:51 +0100

    [01] starting to draw the board

---
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+const TURNS = {
+  X: 'X',
+  O: 'O',
+}
+
+const board = Array(9).fill(null)
+
+function App() {
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <div className='cell' key={index}>
+                <span className='cell__content'>
+                  {index}
+                </span>
+              </div>
+            )
+          })
+        }
+      </section>
+    </main>
+  )
+}
+
+export default App
