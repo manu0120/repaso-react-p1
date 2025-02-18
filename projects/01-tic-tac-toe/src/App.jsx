5b1710709f337fa8171e0ff75f1b6d60a769eaa0
Manuel <manu0120sp@gmail.com>
2025-02-18 23:59:02 +0100

    [01] data persistence with localstorage

--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -3,14 +3,23 @@ import {useState} from 'react'
 import {Square} from './components/Square'
 import confetti from 'canvas-confetti'
 import {TURNS} from './constants'
-import { checkWinnerFrom } from './logic/board'
+import { checkWinnerFrom, checkEndGame } from './logic/board'
 import {WinnerModal} from './components/WinnerModal'
-import { checkEndGame } from './logic/board'
 
 function App() {
-  // uso de useReducer (otra alternativa): https://chatgpt.com/share/6796bc09-0e40-8011-b037-82d97024e66c 
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  // uso de useReducer (otra alternativa): https://chatgpt.com/share/6796bc09-0e40-8011-b037-82d97024e66c
+  // nunca se puede usar un useState dentro de un if, ya que van por posiciones fijas en memoria interna
+  const [board, setBoard] = useState(() => {
+    // se pone dentro porque asi no se accede al localStorage cada vez que se renderiza el componente, por lo que sería mas lento
+    const boardFromLocalStorage = localStorage.getItem('board');
+    return boardFromLocalStorage ? JSON.parse(boardFromLocalStorage) : Array(9).fill(null);
+  })
+  const [turn, setTurn] = useState(() => {
+    const turnFromLocalStorage = localStorage.getItem('turn')
+    /* ||: Reemplaza cualquier valor "falsy", lo que puede no ser deseado si false o 0 son valores válidos que quieres conservar.
+    ??: Solo reemplaza null o undefined, permitiendo que otros valores "falsy" (como false o 0) sean utilizados. */
+    return turnFromLocalStorage ?? TURNS.X
+  })
   // null: no hay ganador; false: empate
   const [winner, setWinner] = useState(null)
 
@@ -38,6 +47,10 @@ function App() {
     
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // guardamos la partida
+    localStorage.setItem('board', JSON.stringify(newBoard))
+    localStorage.setItem('turn', newTurn)
 
     // revisamos si hay ganador
     // a checkWinner no le pasamos el board, sino el board que ya hayamos actualizado con el turno, ya que la actualización del estado no es síncrona, y habría que pasar otra vez más para ver que hay ganador
